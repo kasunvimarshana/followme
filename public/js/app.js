d12f5c2096d06f586619504d0066bc1f7e9c7c8e
kasunvimarshana <kasunvmail@gmail.com>
2019-03-21 16:39:11 +0530

    test

--- public/js/app.js
+++ public/js/app.js
@@ -49207,8 +49207,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\users\kasunv\desktop\Projects\followme\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\users\kasunv\desktop\Projects\followme\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\kasunv\Desktop\Projects\followme\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\kasunv\Desktop\Projects\followme\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
