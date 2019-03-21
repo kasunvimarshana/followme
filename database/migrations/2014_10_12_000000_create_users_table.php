62fc5f19e9ee788f72993c13b3865b6e9a5f7fe3
kasunvimarshana <kasunvmail@gmail.com>
2019-03-21 10:22:52 +0530

    make models and migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,31 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            //default fields
+            //$table->bigIncrements('id');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            //custom fields
+            $table->bigInteger('id')->unsigned()->unique();
+            $table->bigInteger('epf_no')->unsigned()->unique();
+            $table->bigInteger('company')->unsigned();
+            $table->bigInteger('department')->unsigned();
+            $table->bigInteger('user_position')->unsigned()->nullable();
+            $table->bigInteger('created_by')->unsigned();
+            $table->string('phone');
+            $table->boolean('active')->default(0);
+            
+            $table->primary('id');
+            $table->foreign('company')->references('id')->on('companies')->onDelete('cascade')->onUpdate('cascade');
+            $tale->foreign('department')->references('id')->on('departments')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('user_position')->references('id')->on('user_positions')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('created_by')->references('id')->on('sys_users')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('hod')->references('id')->on('sys_users')->onDelete('cascade')->onUpdate('cascade');
+            //$table->index('epf_no');
         });
     }
 
