24d4db86a0ba39d1c473ea749d67a9b79d2c423c
kasunvimarshana <kasunvmail@gmail.com>
2019-05-23 09:18:38 +0530

    edit navigation

--- resources/views/partials/left_side_column.blade.php
+++ resources/views/partials/left_side_column.blade.php
@@ -120,7 +120,7 @@
                             </span>
                         </a>
                         <ul class="treeview-menu">
-                            <li class="{!! set_active(['backstage/users/create', 'backstage/users/create/*']) !!}"><a href="{!! route('meetingCategory.create') !!}"> <i class="fa fa-arrow-circle-o-right"></i> Control</a></li>
+                            <li class="{!! set_active(['backstage/users/create', 'backstage/users/create/*']) !!}"><a href="#"> <i class="fa fa-arrow-circle-o-right"></i> Control</a></li>
                         </ul>
                     </li>
                 </ul>
