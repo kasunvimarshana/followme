faf50914f330f6784646ea57c3ae6d9503a7b401
kasunvimarshana <kasunvmail@gmail.com>
2019-08-23 12:59:23 +0530

    edit ui

--- resources/views/partials/tw_summary/tw_data_table_company_all.blade.php
+++ resources/views/partials/tw_summary/tw_data_table_company_all.blade.php
@@ -243,6 +243,117 @@ $(function(){
                 buttonToolbar.addClass('btn-toolbar');//pull-left
                 
                 //button group
+                var buttonGroup_1 = $('<div></div>');
+                buttonGroup_1.addClass('btn-group');
+                var button_1 = $('<button></button>');
+                button_1.addClass('btn btn-info btn-sm');
+                var button_1_body = $('<i></i>');
+                button_1_body.addClass('fa fa-edit');
+                button_1_body.attr('data-toggle', 'tooltip');
+                button_1_body.attr('data-placement', 'auto');
+                button_1_body.attr('data-container', 'body');
+                //button_1_body.attr('title', 'title');
+                button_1_body.attr('data-title', 'Edit');
+                //button_1_body.attr('data-content', 'content');
+                button_1_body.tooltip();
+                //button_1_body.text('text');
+                button_1.on("click", function(){
+                    var url = "{!! route('tw.edit', ['#tW']) !!}";
+                    url = url.replace("#tW", rowData.id);
+                    //$( location ).attr("href", url);
+                    var windowObject = window.open(url, '_blank', null, true);
+                    windowObject.focus();
+                });
+                button_1.append(button_1_body);
+                buttonGroup_1.append(button_1);
+                
+                //button group
+                var buttonGroup_2 = $('<div></div>');
+                buttonGroup_2.addClass('btn-group');
+                var button_2 = $('<button></button>');
+                button_2.addClass('btn btn-danger btn-sm');
+                var button_2_body = $('<i></i>');
+                button_2_body.addClass('fa fa-trash-o');
+                button_2_body.attr('data-toggle', 'tooltip');
+                button_2_body.attr('data-placement', 'auto');
+                button_2_body.attr('data-container', 'body');
+                //button_2_body.attr('title', 'title');
+                button_2_body.attr('data-title', 'Delete');
+                //button_2_body.attr('data-content', 'content');
+                button_2_body.tooltip();
+                button_2.on("click", function(){
+                    button_2.attr("disabled", true);
+                    bootbox.confirm({
+                        size: "small",
+                        title: "Confirmation",
+                        message: "<strong>Do you want to delete 3W ?</strong><br/><small>" + rowData.title + "</small>",
+                        onEscape: true,
+                        show: true,
+                        scrollable: true,
+                        buttons: {
+                            confirm: {
+                                label: 'Yes',
+                                className: 'btn-success'
+                            },
+                            cancel: {
+                                label: 'No',
+                                className: 'btn-danger btn-primary'
+                            }
+                        },
+                        callback: function (result) {
+                            //console.log('This was logged in the callback: ' + result);
+                            if( result === true ){
+                                var url = "{!! route('tw.destroy', ['#tW']) !!}";
+                                url = url.replace("#tW", rowData.id);
+                                //$( location ).attr("href", url);
+                                
+                                $.ajax({
+                                    type: "GET",
+                                    url: url,
+                                    data: null,
+                                    //success: success,
+                                    //dataType: dataType,
+                                    //context: document.body
+                                })
+                                .done(function( data ) {
+                                    swal({
+                                        'title': data.title,
+                                        'text': data.text,
+                                        'type': data.type,
+                                        'timer': data.timer,
+                                        'showConfirmButton': false
+                                    });
+                                    $('#twDataTable').DataTable().ajax.reload( null, false ); // user paging is not reset on reload
+                                })
+                                .fail(function() {
+                                    //console.log( "error" );
+                                    alert('fail');
+                                })
+                                .always(function() {
+                                    //console.log( "finished" );
+                                    button_2.attr("disabled", false);
+                                });
+                                
+                            }else{
+                                button_2.attr("disabled", false);
+                            }
+                        }
+                    })
+                        .find('.modal-header').addClass('bg-danger')
+                        /*.find('.bootbox-cancel:first').focus()
+                        .find('.bootbox-cancel').attr('autofocus', true)
+                        .on('shown.bs.modal', function(e){
+                            $(this).find(".bootbox-cancel:first").focus();
+                        })*/
+                        .init(function(e){
+                            $(this).find(".bootbox-cancel").focus();
+                        });
+                    
+                });
+                button_2.append(button_2_body);
+                buttonGroup_2.append(button_2);
+                
+                //button group
                 var buttonGroup_3 = $('<div></div>');
                 buttonGroup_3.addClass('btn-group');
                 var button_3 = $('<button></button>');
@@ -350,6 +461,7 @@ $(function(){
                                 })
                                 .fail(function() {
                                     //console.log( "error" );
+                                    alert('fail');
                                 })
                                 .always(function() {
                                     //console.log( "finished" );
@@ -379,22 +491,138 @@ $(function(){
                 var buttonGroup_6 = $('<div></div>');
                 buttonGroup_6.addClass('btn-group');
                 var button_6 = $('<button></button>');
-                button_6.addClass('btn btn-success btn-sm');
+                button_6.addClass('btn btn-info btn-sm');
                 var button_6_body = $('<i></i>');
-                button_6_body.addClass('fa fa-child');
+                button_6_body.addClass('fa fa-refresh');
                 button_6_body.attr('data-toggle', 'tooltip');
                 button_6_body.attr('data-placement', 'auto');
                 button_6_body.attr('data-container', 'body');
                 //button_6_body.attr('title', 'title');
-                button_6_body.attr('data-title', 'Clone');
+                button_6_body.attr('data-title', 'Update Status');
                 //button_6_body.attr('data-content', 'content');
                 button_6_body.tooltip();
                 button_6.on("click", function(){
-                    var url = "{!! route('tw.showClone', ['#tW']) !!}";
-                    url = url.replace("#tW", rowData.id);
-                    //$( location ).attr("href", url);
-                    var windowObject = window.open(url, '_blank', null, true);
-                    windowObject.focus();
+                    button_6.attr("disabled", true);
+                    var formTemplateClone = $('#formTemplate').contents().clone(true);
+                    //var formTemplateCloneChild = formTemplateClone.children().get();
+                    var formTemplateCloneChild = formTemplateClone.children().first();
+                    var formTemplateCloneChildId = 'formTemplateCloneChild' + rowData.id;
+                    formTemplateCloneChild.find('form').attr('id', formTemplateCloneChildId);
+                    
+                    var bootboxObj = bootbox.confirm({
+                        size: "small",
+                        title: "Confirmation",
+                        message: formTemplateCloneChild.html(),
+                        onEscape: true,
+                        show: true,
+                        scrollable: true,
+                        buttons: {
+                            confirm: {
+                                label: 'Yes',
+                                className: 'btn-success',
+                                callback: function(){
+                                    //return false;
+                                }
+                            },
+                            cancel: {
+                                label: 'No',
+                                className: 'btn-danger btn-primary',
+                                callback: function(){
+                                    return false;
+                                }
+                            }
+                        },
+                        callback: function (result) {
+                            //console.log('This was logged in the callback: ' + result);
+                            var formObj = $('#' + formTemplateCloneChildId);
+                            formObj.validator('validate');
+                            var due_date = formObj.find('#due_date');
+                            var description = formObj.find('#description');
+                            var _token = formObj.find("[name='_token']");
+                            var due_date_value = due_date.val();
+                            var description_value = description.val();
+                            var _token_value = _token.val();
+                            
+                            if( (result === true) ){
+                                if( (!$.type(description_value) === 'string') ){
+                                   return false;
+                                }else if( (!moment(due_date_value, 'YYYY-MM-DD HH:mm:ss').isValid()) ){
+                                   return false;
+                                }else if( (description_value.length <= 0) ){
+                                    return false;     
+                                }
+                            }
+                            
+                            description_value = 'Re  Submitted Due To : ' + description_value;
+                            
+                            if( (result === true) ){
+                                var url = "{!! route('tw.changeDoneFalse', ['#tW']) !!}";
+                                url = url.replace("#tW", rowData.id);
+                                //$( location ).attr("href", url);
+                                var formdata = {};
+                                formdata.description =  description_value;
+                                formdata.due_date = due_date_value;
+                                formdata._token = _token_value;
+                                
+                                $.ajax({
+                                    type: "POST",
+                                    url: url,
+                                    data: formdata,
+                                    //success: success,
+                                    //dataType: dataType,
+                                    //context: document.body
+                                })
+                                .done(function( data ) {
+                                    swal({
+                                        'title': data.title,
+                                        'text': data.text,
+                                        'type': data.type,
+                                        'timer': data.timer,
+                                        'showConfirmButton': false
+                                    });
+                                    $('#twDataTable').DataTable().ajax.reload( null, false ); // user paging is not reset on reload
+                                })
+                                .fail(function() {
+                                    //console.log( "error" );
+                                })
+                                .always(function() {
+                                    //console.log( "finished" );
+                                    button_6.attr("disabled", false);
+                                });
+                                
+                            }else{
+                                button_6.attr("disabled", false);
+                            }
+                        }
+                    })
+                        .find('.modal-header').addClass('bg-success')
+                        /*.find('.bootbox-cancel:first').focus()
+                        .find('.bootbox-cancel').attr('autofocus', true)
+                        .on('shown.bs.modal', function(e){
+                            $(this).find(".bootbox-cancel:first").focus();
+                        })*/
+                        .init(function(e){
+                            $(this).find(".bootbox-cancel").focus();
+                            
+                            var formObj = $('#' + formTemplateCloneChildId);
+                            formObj.find('#due_date').datepicker({
+                                'autoclose': true,
+                                'format': "yyyy-mm-dd",
+                                'immediateUpdates': true,
+                                'todayBtn': true,
+                                'todayHighlight': true,
+                                // 'widgetParent': ???,
+                                'widgetPositioning': {
+                                    horizontal: "auto",
+                                    vertical: "auto"
+                                },
+                                'toggleActive': true,
+                                'orientation': 'auto',
+                                'container': 'body'
+                            })
+                                .datepicker("setStartDate", moment(rowData.due_date, 'YYYY-MM-DD HH:mm:ss').toDate())
+                                .datepicker("setDate", moment(rowData.due_date, 'YYYY-MM-DD HH:mm:ss').toDate()).trigger("changeDate");
+                        });
                 });
                 button_6.append(button_6_body);
                 buttonGroup_6.append(button_6);
@@ -572,6 +800,7 @@ $(function(){
                 buttonGroup_8.append(button_8);
                 
                 buttonToolbar.append(buttonGroup_3);
+                /*
                 if( ((rowData.is_done == false) || (rowData.is_done == null)) ){//open
                     //buttonToolbar.append(buttonGroup_4);
                 }
@@ -581,6 +810,7 @@ $(function(){
                 if( ((rowData.is_done == false) || (rowData.is_done == null)) && ((rowData.is_cloned == false) || (rowData.is_cloned == null)) ){//completed
                     //buttonToolbar.append(buttonGroup_6);
                 }
+                */
                 
                 /* php */
                 @superadmin
@@ -589,6 +819,21 @@ $(function(){
                 }else{
                     buttonToolbar.append(buttonGroup_7);
                 }
+                @endsuperadmin
+                /* php */
+                
+                /* php */
+                @superadmin
+                if( ((rowData.is_done == false) || (rowData.is_done == null)) ){//open
+                    buttonToolbar.append(buttonGroup_4);
+                }
+                if( rowData.is_done ){
+                    buttonToolbar.append(buttonGroup_6);
+                }else{
+                    buttonToolbar.append(buttonGroup_5);
+                    buttonToolbar.append(buttonGroup_1);
+                }
+                buttonToolbar.append(buttonGroup_2);
                 @endsuperadmin
                 /* php */
                 
